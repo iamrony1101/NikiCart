af5ad81b8382c30096f1c21fa098863a71e131c7
Movie Booking <abhijeetkumarorlaha620@gmail.com>
2026-02-08 05:24:51 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/NikiCart/NikiCartApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.NikiCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikiCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikiCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/NikiCart/NikiCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.NikiCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikiCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
